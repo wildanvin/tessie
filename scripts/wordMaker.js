357e6df3673aecf8d61ccb8349fd2b3f0901fd17
wildanvin <danvin.jim@gmail.com>
2022-02-16 02:02:56 -0500

    Add wordMaker function

---
+++ scripts/wordMaker.js
@@ -0,0 +1,32 @@
+module.exports = function wordMaker(name, ncFileName) {
+  const template = require("../templates/template1.json")
+  const fs = require("fs")
+  const text = name
+  const widthSlot1 = 30
+
+  var totalWidthOfText = 0
+  for (let i of text) {
+    var totalWidthOfText = totalWidthOfText + template[i].width
+  }
+
+  var spaceInX = (widthSlot1 - totalWidthOfText) / 2
+
+  /**This "double for" space the letters in x an centers them ;) */
+  for (let i of text) {
+    for (let j = 0; j < template[i].values.length; j++) {
+      let a = template[i].values[j]
+      let b = parseFloat(a.match(/(?<=X)\d.\d\d\d/))
+      let c = b + spaceInX
+      let d = a.replace(/(?<=X)\d.\d\d\d/, c.toFixed(3))
+
+      //These 3 lines append the current line to the file:
+
+      fs.appendFileSync(`../ncFiles/${ncFileName}.nc`, d, function (err) {
+        if (err) throw err
+      })
+    }
+    spaceInX = spaceInX + template[i].width
+  }
+}
+
+//export default wordMaker
--- server.js
+++ server.js
@@ -1,51 +1,52 @@
-const express = require("express");
-const path = require("path");
-const { v1: uuidv1 } = require("uuid");
+const express = require("express")
+const path = require("path")
+const { v1: uuidv1 } = require("uuid")
 
-const gCodeGen = require("./scripts/gCodeGen");
-const sendGCode = require("./scripts/sendGCode");
+const gCodeGen = require("./scripts/gCodeGen")
+const sendGCode = require("./scripts/sendGCode")
 
-const app = express();
+const app = express()
 
 // Body parser middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")))
 
 app.post("/engraveSlot1", (req, res) => {
-  const name = req.body.name;
-  global.ncFileName = uuidv1();
-  gCodeGen.engraveSlot1(name, ncFileName);
-  res.end();
-});
+  const name = req.body.name
+  global.ncFileName = uuidv1()
+  gCodeGen.engraveSlot1(name, ncFileName)
+  console.log("hello from slot1 ")
+  res.end()
+})
 
 app.post("/engraveSlot2", (req, res) => {
-  const name = req.body.name;
-  global.ncFileName = uuidv1();
-  gCodeGen.engraveSlot2(name, ncFileName);
-  res.end();
-});
+  const name = req.body.name
+  global.ncFileName = uuidv1()
+  gCodeGen.engraveSlot2(name, ncFileName)
+  res.end()
+})
 
 app.post("/engraveSlot3", (req, res) => {
-  const name = req.body.name;
-  global.ncFileName = uuidv1();
-  gCodeGen.engraveSlot3(name, ncFileName);
-  res.end();
-});
+  const name = req.body.name
+  global.ncFileName = uuidv1()
+  gCodeGen.engraveSlot3(name, ncFileName)
+  res.end()
+})
 
 app.post("/engraveSlot4", (req, res) => {
-  const name = req.body.name;
-  global.ncFileName = uuidv1();
-  gCodeGen.engraveSlot4(name, ncFileName);
-  res.end();
-});
+  const name = req.body.name
+  global.ncFileName = uuidv1()
+  gCodeGen.engraveSlot4(name, ncFileName)
+  res.end()
+})
 
 app.post("/sendGCode", (req, res) => {
-  sendGCode.sendGCode(ncFileName);
-  res.end();
-});
+  sendGCode.sendGCode(ncFileName)
+  res.end()
+})
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
