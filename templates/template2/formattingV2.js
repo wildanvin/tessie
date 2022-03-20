f055b662b5d5119cd1f149f33199571e337d9064
wildanvin <danvin.jim@gmail.com>
2022-03-20 16:21:17 -0500

    template2.json complete

--- templates/template2/formattingV2.js
+++ templates/template2/formattingV2.js
@@ -49,19 +49,52 @@ let letters = [
 ]
 
 var fs = require("fs")
-var fileName = "./A.nc" // path to file
 
-fs.readFile(fileName, "utf8", function (err, data) {
-  let splitArray = data.split("\n")
-  splitArray.splice(-2, 2) //delete last 2 rows
-  splitArray.splice(0, 3) // delete first 3 rows
+const template = require("./template2.json")
 
-  for (let i = 0; i < splitArray.length; i++) {
-    let a = splitArray[i].replace(/(?<=F)\d\d\d.\d/, "200.0") //Change the feed rate to 200.0
-    splitArray[i] = a + "\n" //Add new line at the end
+for (let el of letters) {
+  var fileName = `./${el}.nc` // path to file
+  //var fileName = "./0.nc" // path to file
+
+  console.log(fileName)
+
+  fs.readFile(fileName, "utf8", function (err, data) {
+    let splitArray = data.split("\n")
+    splitArray.splice(-2, 2) //delete last 2 rows
+    splitArray.splice(0, 3) // delete first 3 rows
+
+    for (let i = 0; i < splitArray.length; i++) {
+      let a = splitArray[i].replace(/(?<=F)\d\d\d.\d/, "200.0") //Change the feed rate to 200.0
+      splitArray[i] = a + "\n" //Add new line at the end
+    }
+
+    template[el]["values"] = splitArray
+    //console.log(template)
+    var jsonContent = JSON.stringify(template)
+
+    fs.writeFile("./template2.json", jsonContent, "utf8", function (err) {
+      if (err) {
+        console.log("An error occured while writing JSON Object to File.")
+        return console.log(err)
+      }
+
+      console.log("JSON file has been saved.")
+    })
+  })
+  //console.log(template)
+}
+//console.log(template)
+
+//Now we save the template as a JSON file
+/*
+var jsonContent = JSON.stringify(template)
+
+fs.writeFile("./template2.json", jsonContent, "utf8", function (err) {
+  if (err) {
+    console.log("An error occured while writing JSON Object to File.")
+    return console.log(err)
   }
 
-  var myJSON = JSON.stringify(splitArray)
-
-  console.log(myJSON) //so we can copy the result from the console
+  console.log("JSON file has been saved.")
 })
+*/
